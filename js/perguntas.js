55de6dcc29cad3e3648afd53569259ae041264d7
EDUARDO <114171717+eduardoVARGA@users.noreply.github.com>
2024-09-06 22:27:32 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,172 @@
+export const perguntas = [
+    {
+        enunciado: " Quais são dois benefícios físicos comprovados da prática regular de esportes?",
+        alternativas: [
+            {
+                texto: "Melhora da saúde cardiovascular.",
+                afirmacao: [
+                    "O esporte é uma maneira excelente de promover um estilo de vida saudável e ativo.",
+                    "Praticar esportes regularmente pode ajudar a reduzir o estresse e a ansiedade."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Aumento da flexibilidade e força muscular.",
+                afirmacao: [
+                    "O futebol é o esporte mais popular do mundo, com bilhões de fãs ao redor do globo.",
+                    "A natação é uma atividade completa que trabalha quase todos os músculos do corpo."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "No futebol, quais são duas funções principais de um zagueiro?",
+        alternativas: [
+            {
+                texto: " Impedir que o adversário marque gols.",
+                afirmacao: [
+                    "O basquete desenvolve habilidades importantes como coordenação e trabalho em equipe.",
+                    "O ciclismo não só melhora a condição física, mas também é uma forma de transporte ecológica.",
+                    "A prática de esportes pode melhorar a autoestima e a confiança pessoal."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Auxiliar na construção das jogadas ofensivas a partir da defesa.",
+                afirmacao: [
+                    "Em esportes coletivos, como o vôlei, a comunicação entre os jogadores é crucial para o sucesso.",
+                    "O atletismo é uma forma de esporte que abrange corridas, saltos e lançamentos.",
+                    "O tênis é um esporte que combina habilidades físicas e mentais para alcançar a vitória."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são dois aspectos importantes que uma dieta equilibrada deve ter para apoiar o desempenho esportivo?",
+        alternativas: [
+            {
+                texto: "Ingestão adequada de proteínas para recuperação muscular.",
+                afirmacao: [
+                    "O rugby é conhecido por seu jogo físico intenso e estratégia tática.",
+                    "A ginástica rítmica exige tanto flexibilidade quanto coordenação e graça.",
+                    "O skateboarding é uma forma popular de esporte radical que desafia limites e promove criatividade."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Consumo de carboidratos complexos para fornecer energia sustentada",
+                afirmacao: [
+                    "O boxe desenvolve força, resistência e habilidades de defesa pessoal.",
+                    "Em esportes de equipe, o trabalho em grupo é essencial para atingir objetivos comuns.",
+                    "O hóquei no gelo é um esporte emocionante que mistura velocidade e habilidades com o disco."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "No basquete, quais são duas técnicas fundamentais para uma defesa eficaz?",
+        alternativas: [
+            {
+                texto: " Marcação de pressão alta para forçar erros.",
+                afirmacao: [
+                    "A corrida de maratona é uma das provas de resistência mais desafiadoras e gratificantes.",
+                    "O surfe é um esporte que combina habilidade com uma conexão profunda com a natureza."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Posicionamento adequado para contestar arremessos.",
+                afirmacao: [
+                    "No judô, a técnica e o equilíbrio são mais importantes que a força bruta.",
+                    "O badminton é um esporte rápido que requer agilidade e precisão nos movimentos."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são duas estratégias comuns usadas por equipes de voleibol para aumentar suas chances de vencer uma partida?",
+        alternativas: [
+            {
+                texto: "Realizar um serviço agressivo para dificultar a recepção do adversário.",
+                afirmacao: [
+                    "A esgrima é um esporte de combate que combina rapidez e estratégia.",
+                    "A prática de esportes de combate pode aumentar a disciplina e o autocontrole."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Utilizar bloqueios eficazes para neutralizar ataques adversários.",
+                afirmacao: [
+                    "O esqui é um esporte que proporciona diversão e adrenalina nas montanhas nevadas.",
+                    "O vôlei de praia exige resistência e técnicas específicas devido às condições desafiadoras da areia."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "No atletismo, quais são dois elementos essenciais para o sucesso em corridas de curta distância?",
+        alternativas: [
+            {
+                texto: "Técnica de partida explosiva.",
+                afirmacao: [
+                    "O golfe é um esporte que exige paciência e habilidade em técnicas de precisão.",
+                    "O handebol é conhecido pela sua dinâmica rápida e pelo envolvimento intenso dos jogadores."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Manutenção de alta velocidade durante a corrida.",
+                afirmacao: [
+                    "A ioga pode complementar o treinamento esportivo, melhorando a flexibilidade e a concentração.",
+                    "O levantamento de peso fortalece o corpo e pode ajudar na melhora da postura."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Em esportes aquáticos, como a natação, quais são dois fatores que influenciam diretamente a eficiência do desempenho?",
+        alternativas: [
+            {
+                texto: "Técnica adequada de nado para minimizar a resistência na água.",
+                afirmacao: [
+                    "A patinação artística combina habilidades atléticas com elementos de dança e música.",
+                    "O triatlo é uma prova desafiadora que combina natação, ciclismo e corrida."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Capacidade de manter uma respiração controlada e ritmada.",
+                afirmacao: [
+                    "O karatê é uma arte marcial que enfatiza a técnica e o controle mental.",
+                    "O polo aquático é um esporte coletivo exigente que mistura habilidades de natação e estratégia."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: " No ciclismo de estrada, quais são duas práticas recomendadas para otimizar a performance durante uma competição?",
+        alternativas: [
+            {
+                texto: "Manter uma postura aerodinâmica para reduzir o arrasto do vento.",
+                afirmacao: [
+                    "O badminton é conhecido por seus rápidos reflexos e precisos movimentos de raquete.",
+                    "No tênis de mesa, a velocidade e a precisão são essenciais para dominar a partida."
+                ],
+            },
+            {
+                texto: " Realizar treinos intervalados para melhorar a capacidade cardiovascular e resistência.",
+                afirmacao: [
+                    "O skateboarding promove a criatividade e o desenvolvimento de habilidades técnicas.",
+                    "A prática de esportes individuais pode ajudar a desenvolver a autodisciplina e o foco.",
+                ],
+            },
+        ]
+    }
+];
